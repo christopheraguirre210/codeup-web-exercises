ba7e8ef3a5e775cd3cebdf88c383ceda965b2fbb
Christopher Aguirre <christopher.aguirre210@gmail.com>
2019-10-15 11:40:19 -0500

    loops exercise update

---
+++ js/while.js
@@ -0,0 +1,15 @@
+// var i = 1;
+//
+// while (i < 65536) {
+//     i = i * 2
+//     console.log(i);
+// }
+
+// This is how you get a random number between 50 and 100
+var allCones = Math.floor(Math.random() * 50) + 50;
+// This expression will generate a random number between 1 and 5
+var customer = Math.floor(Math.random() * 5) + 1;
+
+// do{
+//
+// } while(customer < allCones);
