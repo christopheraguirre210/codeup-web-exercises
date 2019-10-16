82c3abeb360b8b8b00d70713caefff3db119cb39
Christopher Aguirre <christopher.aguirre210@gmail.com>
2019-10-16 11:10:53 -0500

    while loops exercise complete oct 16

--- js/while.js
+++ js/while.js
@@ -13,21 +13,22 @@
 var allCones = Math.floor(Math.random() * 50) + 50;
 console.log("Total cones available to sell: " + allCones);
 
-do{
-    var cones_sold = Math.floor(Math.random() * 5) + 1;
 
-    if(allCones > cones_sold){
-        allCones -= cones_sold;
-        console.log(cones_sold + " cones sold. " + allCones + " remaining.");
+do {
+    var customer = Math.floor(Math.random() * 5 + 1);
 
-    }else if(allCones === cones_sold){
-        allCones -= cones_sold;
-        console.log(cones_sold + " cones sold. " + allCones + " remaining.");
-        console.log("All the cones have been sold.");
-        break;
+    if(allCones > customer){
+        allCones -= customer;
+        console.log(customer + " cones sold." + allCones + " cones left to sell.");
 
-    }else if(allCones < cones_sold){
-        console.log("I don't have enough cones to sell you, sorry.");
-    }
+    } else if(allCones === customer){
+            allCones -= customer;
+            console.log(customer + " cones sold." + allCones + " cones left to sell.");
+            console.log("Yay! I sold them all!");
+            break;
 
-} while(allCones >= 0);
+    }   else if(allCones < customer){
+                console.log("I don't have enough cones to sell you.");
+            }
+
+}while(allCones >= 0);
