e3db4364afc7d217e87822329f1b41ba83b38919
Christopher Aguirre <christopher.aguirre210@gmail.com>
2019-10-16 11:36:48 -0500

    break and continue fixed

--- js/break_and_continue.js
+++ js/break_and_continue.js
@@ -1,10 +1,15 @@
-prompt("pick a number between 1 and 50");
 
-for (var i = 1; i <= 50; i++) {
-    if (i % 2 === 0) {
-        continue;
-    } else if (i === 27) {
-        console.log("Yikes! we're skipping the number " + i);
+    do {
+        var userMessage = prompt("pick an odd number between 1 and 50");
+    } while(userMessage % 2 === 0);
+    userMessage = parseFloat(userMessage);
+    console.log("You picked " + userMessage);
+    for (var i = 0; i <= 50; i++) {
+        if (i % 2 !== 0) {
+            if(i === userMessage){
+                console.log("skipping the number " + userMessage);
+                continue;
+            }
+            console.log(i + " is an odd number");
+        }
     }
-    console.log("Here is an odd number: " + i);
-}
