c3fd823bdb36a1b65895cbdb28464f0d07381c53
Christopher Aguirre <christopher.aguirre210@gmail.com>
2019-10-21 13:06:52 -0500

    array-object-bonus progress

--- js/array-object-bonus.js
+++ js/array-object-bonus.js
@@ -5,7 +5,7 @@
 function filterNumbers(arr){
     var newArray = [];
     for(var i = 0;i < arr.length;i++){
-        if(typeof arr[i] == 'number'){
+        if(typeof arr[i] === 'number'){
             return newArray.unshift(arr[i]);
         }
     }
@@ -30,18 +30,80 @@ var dogs = [
     },
     {
         name: "Mr. Pig",
-        breed: "Mastif",
+        breed: "Mastiff",
         age: 10
     }
 ];
 
-function getOlder(arr){
-    for(var i = 0;i < arr.length;i++){
-        if(arr[i] === dogs.age){
-            return arr[i] + 1;
-        }
+console.log(dogs);
+
+function getOlder(arr,index){
+    dogs.forEach(function (dog,index) {
+        dogs[index].age += 1;
+    })
+}
+
+//Write a function, washCars() that takes in a array of car objects and sets the boolean properties of isDirty to false.
+
+var cars = [
+    {
+        make: "Volvo",
+        color: "red",
+        year: "1996",
+        isDirty: true
+    },
+    {
+        make: "Toyota",
+        color: "black",
+        year: "2004",
+        isDirty: false
+    },
+    {
+        make: "Ford",
+        color: "white",
+        year: "2007",
+        isDirty: true
     }
-};
+];
+
+console.log(cars);
+
+function washCars(arr){
+    cars.forEach(function (car,index) {
+        cars[index].isDirty = false;
+    })
+}
+
+//Write a function, adminList() that takes in an array of user objects and returns a count of all admins based on the isAdmin property. Refactor to return an array of admin-only user emails. Refactor again to return an array of user objects that are admins.
+
+var admins = [
+    {
+        isAdmin: true,
+        email: 'user1@email.com'
+    },
+    {
+        isAdmin: true,
+        email: 'user2@email.com'
+    },
+    {
+        isAdmin: false,
+        email: 'user3@email.com'
+    }
+];
+
+console.log(admins);
+
+function adminList(arr){
+    var adminArray = [];
+    admins.forEach(function (admin,index) {
+        if(admins[index].isAdmin === true){
+            return adminArray.unshift(arr[index]);
+        }
+    })
+}
+
+//Create a function, makeSandwichObjects() that takes in two array of strings, breads and fillings and returns an array of sandwichObjects that contain properties for bread and filling and values correspond to the same order of the two passed in arrays. Assume the two array inputs are the same length.
+
 
 
 
