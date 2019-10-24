916702912b8f54587c8a27a669e66ee96b63f6ee
Christopher Aguirre <christopher.aguirre210@gmail.com>
2019-10-24 09:23:01 -0500

    morning warmup

--- js/javascript-warmups.js
+++ js/javascript-warmups.js
@@ -206,3 +206,19 @@ function createPersonObject(name,age){
 
 console.log(createPersonObject());
 
+
+function fizzBuzz(num){
+    for(var i = 1; i <= num; i++){
+        if(i % 15 === 0){
+            console.log("fizzbuzz");
+        }else if(i % 5 === 0){
+            console.log("buzz");
+        }else if(i % 3 === 0){
+            console.log("fizz");
+        }else{
+            console.log(i);
+        }
+    }
+}
+
+console.log(fizzBuzz(100));
