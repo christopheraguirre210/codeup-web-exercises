bb59e7131b829d2a63c92a0c4d75aa8757f1e6d2
Christopher Aguirre <christopher.aguirre210@gmail.com>
2019-10-29 11:27:00 -0500

    first exercise

--- js/javascript-warmups.js
+++ js/javascript-warmups.js
@@ -269,3 +269,43 @@ function mostOranges(carts){
     })
 }
 
+//write a function that accepts an array of student objects and returns an array of student objects that are not registered
+
+var students = [
+    {
+        name: "Mary",
+        grade: 10,
+        gpa: 3.7,
+        isRegistered: true
+    },
+    {
+        name: "Tim",
+        grade: 12,
+        gpa: 3.2,
+        isRegistered: false
+    },
+    {
+        name: "Ellie",
+        grade: 9,
+        gpa: 4.0,
+        isRegistered: false
+    },
+    {
+        name: "Alex",
+        grade: 10,
+        gpa: 3.9,
+        isRegistered: true
+    }
+];
+
+function checkRegistered(studentArray){
+    var newArray = [];
+    studentArray.forEach(function (student) {
+        if(!student.isRegistered){
+            newArray.push(student)
+        }
+    });
+    return newArray;
+}
+
+
